72c09c60c018af574af028a21f972370993426ae
KH178 <karanhanda17821@gmail.com>
2020-02-07 13:14:28 +0530

    added user info in context

--- src/context/UserLoginState.js
+++ src/context/UserLoginState.js
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect } from 'react';
+import React, { createContext, useEffect,useState } from 'react';
 import useToggleState from '../hooks/useToggleState';
 import { useCookies } from 'react-cookie';
 
@@ -6,12 +6,19 @@ export const UserLoginState = createContext();
 
 function UserLoginStateContextProvider(props) {
     const [isUserLogin, setIsUserLogin] = useToggleState(false);
+    const [userName,setUserName] = useState('');
+    const [userEmail,setUserEmail] = useState('');
     const [cookies, setCookie, removeCookie] = useCookies(['cookie-name']);
     if(!isUserLogin){
         
     }
     return(
-    <UserLoginState.Provider value={{ isUserLogin, setIsUserLogin: setIsUserLogin }}>
+    <UserLoginState.Provider value={{ isUserLogin, setIsUserLogin: setIsUserLogin,useInfo:{
+        name: userName,
+        email: userEmail,
+        setName: setUserName,
+        setEmail: setUserEmail
+    } }}>
            {props.children}
     </UserLoginState.Provider>
     )
