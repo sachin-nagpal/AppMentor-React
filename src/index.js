4ff0c6aaae5a2f9264f3384d856ed3837fb9ab14
Karan <karanhanda17821@gmail.com>
2020-02-23 21:29:42 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter ><App/></BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
